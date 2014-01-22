9de2220615d879e920ddac0830142f57b40247b6
OliverD <oliver312@gmail.com>
2014-01-21 21:52:13 -0400

    removing commented code

--- app/scripts/Factories/Factories.js
+++ app/scripts/Factories/Factories.js
@@ -7,48 +7,12 @@ app.factory('heroesFactory', ['$http', function($http) {
         return $http.get(urlBase);
     };
 
-    // heroesFactory.getHero = function (id) {
-    //     return $http.get(urlBase + '/' + id);
-    // };
-
-    // heroesFactory.insertHero = function (hero) {
-    //     return $http.post(urlBase, hero);
-    // };
-
-    // heroesFactory.updateHero = function (hero) {
-    //     return $http.put(urlBase + '/' + hero.id, hero);
-    // };
-
-    // heroesFactory.deleteHero = function (id) {
-    //     return $http.delete(urlBase + '/' + id);
-    // };
-
     return heroesFactory;
 }]);
 
 app.factory('jobsFactory', ['$http', function($http) {
     var urlBase = app.apiURL+'/jobs';
     var jobsFactory = new CrudService($http,urlBase,'Job');
-
-    // jobsFactory.getJobs = function () {
-    //     return $http.get(urlBase);
-    // };
-
-    // jobsFactory.getJob = function (id) {
-    //     return $http.get(urlBase + '/' + id);
-    // };
-
-    // jobsFactory.insertJob = function (job) {
-    //     return $http.post(urlBase, job);
-    // };
-
-    // jobsFactory.updateJob = function (job) {
-    //     return $http.put(urlBase + '/' + job.id, job);
-    // };
-
-    // jobsFactory.deleteJob = function (id) {
-    //     return $http.delete(urlBase + '/' + id);
-    // };
 
     return jobsFactory;
 }]);
@@ -57,26 +21,6 @@ app.factory('racesFactory', ['$http', function($http) {
 
     var urlBase = app.apiURL+'/races';
     var racesFactory = new CrudService($http,urlBase,'Race');;
-
-    // racesFactory.getRaces = function () {
-    //     return $http.get(urlBase);
-    // };
-
-    // racesFactory.getRace = function (id) {
-    //     return $http.get(urlBase + '/' + id);
-    // };
-
-    // racesFactory.insertRace = function (Race) {
-    //     return $http.post(urlBase, Race);
-    // };
-
-    // racesFactory.updateRace = function (Race) {
-    //     return $http.put(urlBase + '/' + Race.id, Race);
-    // };
-
-    // racesFactory.deleteRace = function (id) {
-    //     return $http.delete(urlBase + '/' + id);
-    // };
 
     return racesFactory;
 }]);
