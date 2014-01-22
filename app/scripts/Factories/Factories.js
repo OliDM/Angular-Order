3bb8f01f01b31a64e810ad140cff3af474b48c9a
OliverD <oliver312@gmail.com>
2014-01-21 21:13:32 -0400

    refactoring the basic CRUD operations

--- app/scripts/Factories/Factories.js
+++ app/scripts/Factories/Factories.js
@@ -1,53 +1,54 @@
 app.factory('heroesFactory', ['$http', function($http) {
 
     var urlBase = app.apiURL+'/heroes';
-    var heroesFactory = {};
+    var heroesFactory = new CrudService($http,urlBase,'Hero');;
 
     heroesFactory.getHeroes = function () {
         return $http.get(urlBase);
     };
 
-    heroesFactory.getHero = function (id) {
-        return $http.get(urlBase + '/' + id);
-    };
+    // heroesFactory.getHero = function (id) {
+    //     return $http.get(urlBase + '/' + id);
+    // };
 
-    heroesFactory.insertHero = function (hero) {
-        return $http.post(urlBase, hero);
-    };
+    // heroesFactory.insertHero = function (hero) {
+    //     return $http.post(urlBase, hero);
+    // };
 
-    heroesFactory.updateHero = function (hero) {
-        return $http.put(urlBase + '/' + hero.id, hero);
-    };
+    // heroesFactory.updateHero = function (hero) {
+    //     return $http.put(urlBase + '/' + hero.id, hero);
+    // };
 
-    heroesFactory.deleteHero = function (id) {
-        return $http.delete(urlBase + '/' + id);
-    };
+    // heroesFactory.deleteHero = function (id) {
+    //     return $http.delete(urlBase + '/' + id);
+    // };
 
     return heroesFactory;
 }]);
 
 app.factory('jobsFactory', ['$http', function($http) {
     var urlBase = app.apiURL+'/jobs';
-    var jobsFactory = {};
-    jobsFactory.getJobs = function () {
-        return $http.get(urlBase);
-    };
+    var jobsFactory = new CrudService($http,urlBase,'Job');
 
-    jobsFactory.getJob = function (id) {
-        return $http.get(urlBase + '/' + id);
-    };
+    // jobsFactory.getJobs = function () {
+    //     return $http.get(urlBase);
+    // };
 
-    jobsFactory.insertJob = function (job) {
-        return $http.post(urlBase, job);
-    };
+    // jobsFactory.getJob = function (id) {
+    //     return $http.get(urlBase + '/' + id);
+    // };
 
-    jobsFactory.updateJob = function (job) {
-        return $http.put(urlBase + '/' + job.id, job);
-    };
+    // jobsFactory.insertJob = function (job) {
+    //     return $http.post(urlBase, job);
+    // };
 
-    jobsFactory.deleteJob = function (id) {
-        return $http.delete(urlBase + '/' + id);
-    };
+    // jobsFactory.updateJob = function (job) {
+    //     return $http.put(urlBase + '/' + job.id, job);
+    // };
+
+    // jobsFactory.deleteJob = function (id) {
+    //     return $http.delete(urlBase + '/' + id);
+    // };
 
     return jobsFactory;
 }]);
@@ -55,27 +56,27 @@ app.factory('jobsFactory', ['$http', function($http) {
 app.factory('racesFactory', ['$http', function($http) {
 
     var urlBase = app.apiURL+'/races';
-    var racesFactory = {};
+    var racesFactory = new CrudService($http,urlBase,'Race');;
 
-    racesFactory.getRaces = function () {
-        return $http.get(urlBase);
-    };
+    // racesFactory.getRaces = function () {
+    //     return $http.get(urlBase);
+    // };
 
-    racesFactory.getRace = function (id) {
-        return $http.get(urlBase + '/' + id);
-    };
+    // racesFactory.getRace = function (id) {
+    //     return $http.get(urlBase + '/' + id);
+    // };
 
-    racesFactory.insertRace = function (Race) {
-        return $http.post(urlBase, Race);
-    };
+    // racesFactory.insertRace = function (Race) {
+    //     return $http.post(urlBase, Race);
+    // };
 
-    racesFactory.updateRace = function (Race) {
-        return $http.put(urlBase + '/' + Race.id, Race);
-    };
+    // racesFactory.updateRace = function (Race) {
+    //     return $http.put(urlBase + '/' + Race.id, Race);
+    // };
 
-    racesFactory.deleteRace = function (id) {
-        return $http.delete(urlBase + '/' + id);
-    };
+    // racesFactory.deleteRace = function (id) {
+    //     return $http.delete(urlBase + '/' + id);
+    // };
 
     return racesFactory;
 }]);
@@ -83,27 +84,7 @@ app.factory('racesFactory', ['$http', function($http) {
 app.factory('weaponsFactory', ['$http', function($http) {
 
     var urlBase = app.apiURL+'/weapons';
-    var weaponsFactory = {};
-
-    weaponsFactory.getWeapons = function () {
-        return $http.get(urlBase);
-    };
-
-    weaponsFactory.getWeapon = function (id) {
-        return $http.get(urlBase + '/' + id);
-    };
-
-    weaponsFactory.insertWeapon = function (Weapon) {
-        return $http.post(urlBase, Weapon);
-    };
-
-    weaponsFactory.updateWeapon = function (Weapon) {
-        return $http.put(urlBase + '/' + Weapon.id, Weapon);
-    };
-
-    weaponsFactory.deleteWeapon = function (id) {
-        return $http.delete(urlBase + '/' + id);
-    };
+    var weaponsFactory = new CrudService($http,urlBase,'Weapon');
 
     return weaponsFactory;
 }]);
@@ -147,3 +128,25 @@ return fadingFact;
 }])
 
 
+function CrudService(http,url,name){
+    this['get'+name+'s'] = function () {
+        return http.get(url);
+    };
+
+    this['get'+name] = function (id) {
+        return http.get(url + '/' + id);
+    };
+
+    this['insert'+name] = function (item) {
+        return http.post(url, item);
+    };
+
+    this['update'+name] = function (item) {
+        return http.put(url + '/' + item.id, item);
+    };
+
+    this['delete'+name] = function (id) {
+        return http.delete(url + '/' + id);
+    };
+    return this;
+}
